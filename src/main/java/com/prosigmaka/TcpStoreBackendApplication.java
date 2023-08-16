fa4100c7ef86d64735c1ca5b3f0143f4c05d7bee
Ryno Andryano <rynoandryano@gmail.com>
2023-08-16 19:04:02 +0700

    Initial commit

---
+++ src/main/java/com/prosigmaka/TcpStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prosigmaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TcpStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TcpStoreBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prosigmaka/TcpStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prosigmaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TcpStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
